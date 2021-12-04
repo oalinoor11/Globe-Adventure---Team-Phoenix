b985c27fa88175b944a4479c4d26298dae7a5d73
shihabflutter <shihab@aapbd.com>
2021-12-05 02:11:04 +0530

    HomeScreen

--- lib/Presentation/AllScreen/Questions/HomeScreen.dart
+++ lib/Presentation/AllScreen/Questions/HomeScreen.dart
@@ -15,133 +15,178 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Container(),
-              //const SizedBox(height: 100.0),
-              const Image(
-                image: AssetImage("assets/banner.png"),
-                width: 300,
-                height: 300,
-                alignment: Alignment.center,
-              ),
-
-
-
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Container(),
-                        RaisedButton(
-                          color: Colors.red,
-                          textColor: Colors.white,
-                          child: Container(
-                            height: 120.0,
-                            width: 120,
-                            child: Center(
-                              child: Text(
-                                "না",
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(24.0),
-                          ),
-                          onPressed: ()
-                          {
-                            print("clicked No");
-                          },
-                        ),
-
-                        const SizedBox(width: 10.0),
-                        RaisedButton(
-                          color: Colors.green,
-                          textColor: Colors.white,
-                          child: Container(
-                            height: 120.0,
-                            width: 120,
-                            child: Center(
-                              child: Text(
-                                "না",
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(24.0),
-                          ),
-                          onPressed: ()
-                          {
-                            print("clicked No");
-                          },
-                        ),
-                      ],
+        child: SingleChildScrollView(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.center,
+              children: [Container(),
+                //const SizedBox(height: 100.0),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: const Image(
+                      image: AssetImage("assets/banner1.png"),
+                      alignment: Alignment.center,
+                      height: 180,
                     ),
-
-                    const SizedBox(height: 10.0),
-
-                    Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Container(),
-                        RaisedButton(
-                          color: Colors.green,
-                          textColor: Colors.white,
-                          child: Container(
-                            height: 120.0,
-                            width: 120,
-                            child: Center(
-                              child: Text(
-                                "না",
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(24.0),
-                          ),
-                          onPressed: ()
-                          {
-                            print("clicked No");
-                          },
-                        ),
-
-                        const SizedBox(width: 10.0),
-                        RaisedButton(
-                          color: Colors.red,
-                          textColor: Colors.white,
-                          child: Container(
-                            height: 120.0,
-                            width: 120,
-                            child: Center(
-                              child: Text(
-                                "না",
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(24.0),
-                          ),
-                          onPressed: ()
-                          {
-                            print("clicked No");
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-        ],
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Container(),
+                          RaisedButton(
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: Container(
+                              height: 120.0,
+                              width: 120,
+                              child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.apartment,
+                                    color: Colors.white,
+                                    size: 40.0,
+                                  ),
+                                  const SizedBox(height: 5.0),
+                                  Text(
+                                      "University",
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(24.0),
+                            ),
+                            onPressed: ()
+                            {
+                              print("clicked university");
+                            },
+                          ),
+
+                          const SizedBox(width: 10.0),
+                          RaisedButton(
+                            color: Colors.green,
+                            textColor: Colors.white,
+                            child: Container(
+                              height: 120.0,
+                              width: 120,
+                              child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.school,
+                                    color: Colors.white,
+                                    size: 40.0,
+                                  ),
+                                  const SizedBox(height: 5.0),
+                                  Text(
+                                    "Courses",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(24.0),
+                            ),
+                            onPressed: ()
+                            {
+                              print("clicked courses");
+                            },
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 10.0),
+
+                      Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Container(),
+                          RaisedButton(
+                            color: Colors.green,
+                            textColor: Colors.white,
+                            child: Container(
+                              height: 120.0,
+                              width: 120,
+                              child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.summarize,
+                                    color: Colors.white,
+                                    size: 40.0,
+                                  ),
+                                  const SizedBox(height: 5.0),
+                                  Text(
+                                    "Preparation",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(24.0),
+                            ),
+                            onPressed: ()
+                            {
+                              print("clicked preparation");
+                            },
+                          ),
+
+                          const SizedBox(width: 10.0),
+                          RaisedButton(
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: Container(
+                              height: 120.0,
+                              width: 120,
+                              child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.psychology,
+                                    color: Colors.white,
+                                    size: 40.0,
+                                  ),
+                                  const SizedBox(height: 5.0),
+                                  Text(
+                                    "Career Coach",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(24.0),
+                            ),
+                            onPressed: ()
+                            {
+                              print("clicked careercoach");
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 30.0),
+
+                const Image(
+                  image: AssetImage("assets/coppedlogo.png"),
+                  alignment: Alignment.center,
+                  height: 80,
+                ),
+          ],
+          ),
         ),
       ),
     );
