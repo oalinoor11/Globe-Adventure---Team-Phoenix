38bbe9dc41f48ecb9151baedf692d19df79cbd0b
shihabflutter <shihab@aapbd.com>
2021-12-05 00:16:41 +0530

    banner added

--- lib/Presentation/AllScreen/Questions/HomeScreen.dart
+++ lib/Presentation/AllScreen/Questions/HomeScreen.dart
@@ -6,21 +6,22 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(backgroundColor: Colors.green,
+        centerTitle: true,
+        title: Text(
+          "Borno Bangla",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center,
             children: [Container(),
-        const Text(
-          "শরীর ব্যাথা অনুভব করছেন ?",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-
-              const SizedBox(height: 100.0),
+              //const SizedBox(height: 100.0),
               const Image(
-                image: AssetImage("assets/logo.png"),
-                width: 200,
-                height: 200,
+                image: AssetImage("assets/banner.png"),
+                width: 300,
+                height: 300,
                 alignment: Alignment.center,
               ),
 
@@ -28,57 +29,114 @@ class HomeScreen extends StatelessWidget {
 
               Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Container(),
-                    const SizedBox(height: 150.0),
-                    RaisedButton(
-                      color: Colors.red,
-                      textColor: Colors.white,
-                      child: Container(
-                        height: 50.0,
-                        width: 100,
-                        child: Center(
-                          child: Text(
-                            "না",
-                            style: TextStyle(
-                              fontSize: 18.0,
+                child: Column(
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Container(),
+                        RaisedButton(
+                          color: Colors.red,
+                          textColor: Colors.white,
+                          child: Container(
+                            height: 120.0,
+                            width: 120,
+                            child: Center(
+                              child: Text(
+                                "না",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
                             ),
                           ),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(24.0),
+                          ),
+                          onPressed: ()
+                          {
+                            print("clicked No");
+                          },
                         ),
-                      ),
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(24.0),
-                      ),
-                      onPressed: ()
-                      {
-                        print("clicked No");
-                      },
+
+                        const SizedBox(width: 10.0),
+                        RaisedButton(
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          child: Container(
+                            height: 120.0,
+                            width: 120,
+                            child: Center(
+                              child: Text(
+                                "না",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(24.0),
+                          ),
+                          onPressed: ()
+                          {
+                            print("clicked No");
+                          },
+                        ),
+                      ],
                     ),
 
-                    const SizedBox(width: 10.0),
-                    RaisedButton(
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Container(
-                        height: 50.0,
-                        width: 100,
-                        child: Center(
-                          child: Text(
-                            "হ্যাঁ",
-                            style: TextStyle(
-                              fontSize: 18.0,
+                    const SizedBox(height: 10.0),
+
+                    Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Container(),
+                        RaisedButton(
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          child: Container(
+                            height: 120.0,
+                            width: 120,
+                            child: Center(
+                              child: Text(
+                                "না",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
                             ),
                           ),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(24.0),
+                          ),
+                          onPressed: ()
+                          {
+                            print("clicked No");
+                          },
                         ),
-                      ),
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(24.0),
-                      ),
-                      onPressed: ()
-                      {
-                        print("clicked Yes");
-                       // Get.toNamed(AppRoutes.SECONDQUESTION);
-                      },
+
+                        const SizedBox(width: 10.0),
+                        RaisedButton(
+                          color: Colors.red,
+                          textColor: Colors.white,
+                          child: Container(
+                            height: 120.0,
+                            width: 120,
+                            child: Center(
+                              child: Text(
+                                "না",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(24.0),
+                          ),
+                          onPressed: ()
+                          {
+                            print("clicked No");
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 ),
