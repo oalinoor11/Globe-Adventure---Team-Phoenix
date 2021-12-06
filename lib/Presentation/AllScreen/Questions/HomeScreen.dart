1bef9575bf86c8f93f855e638891088438300322
shihabflutter <shihab@aapbd.com>
2021-12-06 10:33:39 +0530

    banner paused

--- lib/Presentation/AllScreen/Questions/HomeScreen.dart
+++ lib/Presentation/AllScreen/Questions/HomeScreen.dart
@@ -1,15 +1,7 @@
-// import 'package:BornoBangla/Core/AppRoutes.dart';
 import 'package:flutter/material.dart';
-// import 'package:get/get_core/src/get_main.dart';
-// import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:carousel_pro/carousel_pro.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+//import 'package:carousel_pro/carousel_pro.dart';
 
 class HomeScreen extends StatelessWidget {
-  // final List items = [
-  //   Image.asset("assets/banner1.png"),
-  //   Image.asset("assets/logo.png")
-  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,42 +15,43 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: SingleChildScrollView(
+      body:
+      SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(
               height: 180,
               width: double.infinity,
-              child: Carousel(
-                autoplayDuration: Duration(seconds: 1),
-                animationDuration: Duration(milliseconds: 500),
-                dotBgColor: Colors.transparent,
-                dotSize: 4.0,
-                dotSpacing: 15.0,
-                images: [
-                  Image.asset(
-                    "assets/banner1.png",
-                    fit: BoxFit.cover,
-                  ),
-                  Image.asset(
-                    "assets/banner1.png",
-                    fit: BoxFit.cover,
-                  ),
-                  Image.asset(
-                    "assets/banner1.png",
-                    fit: BoxFit.cover,
-                  ),
-                  Image.asset(
-                    "assets/banner1.png",
-                    fit: BoxFit.cover,
-                  ),
-                ],
-              ),
+              child:
+                Image.asset("assets/banner1.png", fit: BoxFit.cover),
+              // Carousel(
+              //   autoplayDuration: Duration(seconds: 1),
+              //   animationDuration: Duration(milliseconds: 500),
+              //   dotBgColor: Colors.transparent,
+              //   dotSize: 4.0,
+              //   dotSpacing: 15.0,
+              //   images: [
+              //     Image.asset(
+              //       "assets/banner1.png",
+              //       fit: BoxFit.cover,
+              //     ),
+              //     Image.asset(
+              //       "assets/banner1.png",
+              //       fit: BoxFit.cover,
+              //     ),
+              //     Image.asset(
+              //       "assets/banner1.png",
+              //       fit: BoxFit.cover,
+              //     ),
+              //     Image.asset(
+              //       "assets/banner1.png",
+              //       fit: BoxFit.cover,
+              //     ),
+              //   ],
+              // ),
             ),
 
 
-
-          
           ],
         ),
       ),
