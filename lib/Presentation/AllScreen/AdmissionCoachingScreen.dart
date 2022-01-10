f3eefbdeb214c243023cca0889545a162c02522d
oalinoor11 <oalinoor11@gmail.com>
2022-01-10 10:21:42 +0600

    coolalert added for delete coaching

--- lib/Presentation/AllScreen/AdmissionCoachingScreen.dart
+++ lib/Presentation/AllScreen/AdmissionCoachingScreen.dart
@@ -104,38 +104,35 @@ class _AdmissionCoachingScreenState extends State<AdmissionCoachingScreen> {
                               onTap: () {
                                 Get.toNamed(AppRoutes.UCCSCREEN);
                               },
-                              // onLongPress: () async {
-                              //
-                              //   var ADMISSIONCOACHING = FirebaseFirestore.instance.collection('image');
-                              //   await ADMISSIONCOACHING.doc().delete();
-                              //
-                              //   var result = CoolAlert.show(
-                              //     backgroundColor: Colors.green,
-                              //     confirmBtnColor: Colors.red,
-                              //     confirmBtnText: ("Delete"),
-                              //     width: 10,
-                              //     context: context,
-                              //     type: CoolAlertType.confirm,
-                              //     onCancelBtnTap: () =>
-                              //         Get.back(result: false),
-                              //     onConfirmBtnTap: () =>
-                              //         Get.back(result: true),
-                              //   );
-                              //   if (result) {
-                              //     var response =
-                              //         await MaterialRepository()
-                              //         .deleteMaterial(e);
-                              //     if (response) {
-                              //       await CoolAlert.show(
-                              //           context: context,
-                              //           type: CoolAlertType.success);
-                              //     } else {
-                              //       await CoolAlert.show(
-                              //           context: context,
-                              //           type: CoolAlertType.error);
-                              //     }
-                              //   }
-                              // },
+                              onLongPress: () {
+
+                                var result = CoolAlert.show(
+                                  backgroundColor: Colors.green,
+                                  confirmBtnColor: Colors.red,
+                                  confirmBtnText: ("Delete"),
+                                  width: 10,
+                                  context: context,
+                                  type: CoolAlertType.confirm,
+                                  onCancelBtnTap: () =>
+                                      Get.back(result: false),
+                                  onConfirmBtnTap: () =>
+                                      Get.back(result: true),
+                                );
+                                // if (result) {
+                                //   var response =
+                                //       await MaterialRepository()
+                                //       .deleteMaterial(e);
+                                //   if (response) {
+                                //     await CoolAlert.show(
+                                //         context: context,
+                                //         type: CoolAlertType.success);
+                                //   } else {
+                                //     await CoolAlert.show(
+                                //         context: context,
+                                //         type: CoolAlertType.error);
+                                //   }
+                                // }
+                              },
                             );
                           });
                   } else {
