306ae93534c6dc783a73aafa57b1eacc97d4122f
oalinoor11 <oalinoor11@gmail.com>
2022-03-13 02:53:56 +0600

    profile screen initiated

--- lib/Presentation/AllScreen/Profile/ProfileScreen.dart
+++ lib/Presentation/AllScreen/Profile/ProfileScreen.dart
@@ -9,54 +9,127 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: Colors.green,
-        centerTitle: true,
-        title: Center(
-          child: Text(
-            "Profile",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        automaticallyImplyLeading: false,
-      ),
+      backgroundColor: Colors.blueGrey.withOpacity(0.2),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(18.0),
         child: SingleChildScrollView(
           child: Column(crossAxisAlignment: CrossAxisAlignment.center,
             children: [Container(),
-              const SizedBox(height: 200.0),
-              InkWell(
-                child: Container(
-                  height: 50.0,
-                  width: 100.0,
-                  decoration: new BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    boxShadow: [
-                      new BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        blurRadius: 5.0,
+              const SizedBox(height: 40.0),
+              Container(
+                width: double.infinity,
+                height: 130.0,
+                decoration: new BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 20.0),
+                    Container(
+                      height: 80,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset(
+                          "assets/shahed.png",
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ],
-                    borderRadius: BorderRadius.circular(15),
+                    ),
+                    const SizedBox(width: 20.0),
+                    Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Shahed Oali Noor", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 10.0),
+                        Text("oalinoor11@gmail.com", style: TextStyle(fontSize: 12, color: Colors.black87)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15.0),
+              Container(
+                width: double.infinity,
+                height: 280.0,
+                decoration: new BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15),
 
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Center(
-                      child: Text(
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "vjnfx",
+                      style: TextStyle(
+                          fontSize: 16.0, color: Colors.red, fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15.0),
+              Container(
+                width: double.infinity,
+                height: 90.0,
+                decoration: new BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15),
+
+                ),
+                  child: Row(
+                    children: [
+                      Text(
                         "Log Out",
                         style: TextStyle(
-                          fontSize: 16.0,
+                            fontSize: 16.0, color: Colors.red, fontWeight: FontWeight.bold
                         ),
+                      ),
+                    ],
+                  ),
+              ),
+              const SizedBox(height: 15.0),
+              Container(
+                width: double.infinity,
+                height: 60.0,
+                decoration: new BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15),
+
+                ),
+                child: TextButton(
+                  onPressed: (){
+                    FirebaseAuth.instance.signOut();
+                    Get.offAllNamed(AppRoutes.SIGNINSCREEN);
+                  },
+                  child: Center(
+                    child: Text(
+                      "Log Out",
+                      style: TextStyle(
+                          fontSize: 16.0, color: Colors.red, fontWeight: FontWeight.bold
                       ),
                     ),
                   ),
                 ),
-                onTap: (){
-                  FirebaseAuth.instance.signOut();
-                  Get.offAllNamed(AppRoutes.SIGNINSCREEN);
-                },
               ),
             ],
           ),
