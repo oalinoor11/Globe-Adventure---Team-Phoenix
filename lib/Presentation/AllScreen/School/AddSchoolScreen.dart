6ad61e398e831dc181bf9f37a18f79e082f66fdb
oalinoor11 <oalinoor11@gmail.com>
2022-03-18 12:19:46 +0600

    image field size corrected and size suggestion

--- lib/Presentation/AllScreen/School/AddSchoolScreen.dart
+++ lib/Presentation/AllScreen/School/AddSchoolScreen.dart
@@ -108,8 +108,8 @@ class _AddSchoolScreenState extends State<AddSchoolScreen> {
                   }
                 },
                 child: Container(
-                  height: 65,
-                  width: double.infinity,
+                  height: 100,
+                  width: 100,
                   decoration: BoxDecoration(
                     border: Border.all(
                       color: Colors.grey,
@@ -119,13 +119,12 @@ class _AddSchoolScreenState extends State<AddSchoolScreen> {
                     borderRadius: BorderRadius.circular(8),
                   ),
                   child: _image == null
-                      ? Row(
-                          children: [
-                            SizedBox(width: 10),
-                            Text("School Image",
+                      ? Column(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("School Image",
                                 style: TextStyle(
-                                    color: Colors.black, fontSize: 16)),
-                            SizedBox(width: 10),
+                                    color: Colors.black, fontSize: 12)),
+                            SizedBox(height: 10),
                             Icon(
                               Icons.add_a_photo,
                               size: 20,
