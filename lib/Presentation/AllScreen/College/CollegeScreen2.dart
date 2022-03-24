c7e86b035a1484d8fee59eca00ec986e926941bb
oalinoor11 <oalinoor11@gmail.com>
2022-03-24 17:22:46 +0600

    banner scroll off on college2

--- lib/Presentation/AllScreen/College/CollegeScreen2.dart
+++ lib/Presentation/AllScreen/College/CollegeScreen2.dart
@@ -41,27 +41,10 @@ class _CollegeScreen2State extends State<CollegeScreen2> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(),
-            CarouselSlider(
-              options: CarouselOptions(
-                viewportFraction: 1.0,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
+            Container(
+              child: Image(
+                image: NetworkImage(country.bannerImages),
               ),
-              items: [1].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: double.infinity,
-                      child: Image.asset(
-                        "assets/collegebanner2.png",
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
             ),
             const SizedBox(height: 18.0),
             StreamBuilder(
