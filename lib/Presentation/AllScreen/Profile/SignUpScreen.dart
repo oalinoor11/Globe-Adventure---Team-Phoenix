f4760a7d80546c4fe74aef0145040e253a481d44
oalinoor11 <oalinoor11@gmail.com>
2022-03-29 12:01:26 +0600

    signupscreen responsive done

--- lib/Presentation/AllScreen/Profile/SignUpScreen.dart
+++ lib/Presentation/AllScreen/Profile/SignUpScreen.dart
@@ -3,6 +3,7 @@ import 'package:BornoBangla/Core/AppRoutes.dart';
 import 'package:BornoBangla/Data/Models/profile_model.dart';
 import 'package:BornoBangla/Presentation/Controllers/profile_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_navigation/src/snackbar/snackbar.dart';
@@ -34,178 +35,183 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(),
-              SizedBox(
-                height: 80.0,
-              ),
-              Image(
-                image: AssetImage("assets/coppedlogo.png"),
-                height: 180,
-                alignment: Alignment.center,
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    TextField(
-                      controller: nameTextEditingController,
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                        labelText: "Name",
-                        labelStyle: TextStyle(fontSize: 14.0),
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10.0,
+          child: Center(
+            child: Container(
+              width: context.width > 550 ? 550 : double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Image(
+                    image: AssetImage("assets/coppedlogo.png"),
+                    height: 180,
+                    alignment: Alignment.center,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 5.0,
                         ),
-                      ),
-                      style: TextStyle(
-                        fontSize: 14.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    TextField(
-                      controller: phoneTextEditingController,
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                        labelText: "Phone",
-                        labelStyle: TextStyle(
-                          fontSize: 14.0,
-                          fontFamily: "bolt-semibold",
-                        ),
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10.0,
-                        ),
-                      ),
-                      style: TextStyle(
-                        fontSize: 14.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    TextField(
-                      controller: emailTextEditingController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          fontSize: 14.0,
-                          fontFamily: "bolt-semibold",
-                        ),
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10.0,
-                        ),
-                      ),
-                      style: TextStyle(
-                        fontSize: 14.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    TextField(
-                      controller: passwordTextEditingController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          fontSize: 14.0,
-                          fontFamily: "bolt-semibold",
-                        ),
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10.0,
-                        ),
-                      ),
-                      style: TextStyle(
-                        fontSize: 14.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    RaisedButton(
-                      color: Colors.green,
-                      textColor: Colors.white,
-                      child: Container(
-                        height: 50.0,
-                        child: Center(
-                          child: Text(
-                            "Create Account",
-                            style: TextStyle(fontSize: 18.0),
+                        TextField(
+                          controller: nameTextEditingController,
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            labelText: "Name",
+                            labelStyle: TextStyle(fontSize: 14.0),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                          style: TextStyle(
+                            fontSize: 14.0,
                           ),
                         ),
-                      ),
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(24.0),
-                      ),
-                      onPressed: () async {
-                        if (nameTextEditingController.text.length < 3) {
-                          Get.snackbar(
-                            "Error!",
-                            "Enter your name",
-                            snackPosition: SnackPosition.BOTTOM,
-                            backgroundColor: Colors.red,
-                            colorText: Colors.white,
-                          );
-                        } else if (phoneTextEditingController.text.isEmpty) {
-                          Get.snackbar(
-                            "Phone Number Required!",
-                            "Enter your phone number",
-                            snackPosition: SnackPosition.BOTTOM,
-                            backgroundColor: Colors.red,
-                            colorText: Colors.white,
-                          );
-                        } else if (!emailTextEditingController.text
-                            .contains("@")) {
-                          Get.snackbar(
-                            "Invalid Email!",
-                            "Enter correct email address",
-                            snackPosition: SnackPosition.BOTTOM,
-                            backgroundColor: Colors.red,
-                            colorText: Colors.white,
-                          );
-                        } else if (passwordTextEditingController.text.length <
-                            6) {
-                          Get.snackbar(
-                            "Error!",
-                            "Password must be atleast 6 character",
-                            snackPosition: SnackPosition.BOTTOM,
-                            backgroundColor: Colors.red,
-                            colorText: Colors.white,
-                          );
-                        } else {
-                          registerNewUser(context);
-                        }
-                      },
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        TextField(
+                          controller: phoneTextEditingController,
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            labelText: "Phone",
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                              fontFamily: "bolt-semibold",
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        TextField(
+                          controller: emailTextEditingController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                              fontFamily: "bolt-semibold",
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        TextField(
+                          controller: passwordTextEditingController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                              fontFamily: "bolt-semibold",
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        RaisedButton(
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          child: Container(
+                            height: 50.0,
+                            child: Center(
+                              child: Text(
+                                "Create Account",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                            ),
+                          ),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(24.0),
+                          ),
+                          onPressed: () async {
+                            if (nameTextEditingController.text.length < 3) {
+                              Get.snackbar(
+                                "Error!",
+                                "Enter your name",
+                                snackPosition: SnackPosition.BOTTOM,
+                                backgroundColor: Colors.red,
+                                colorText: Colors.white,
+                              );
+                            } else if (phoneTextEditingController.text.isEmpty) {
+                              Get.snackbar(
+                                "Phone Number Required!",
+                                "Enter your phone number",
+                                snackPosition: SnackPosition.BOTTOM,
+                                backgroundColor: Colors.red,
+                                colorText: Colors.white,
+                              );
+                            } else if (!emailTextEditingController.text
+                                .contains("@")) {
+                              Get.snackbar(
+                                "Invalid Email!",
+                                "Enter correct email address",
+                                snackPosition: SnackPosition.BOTTOM,
+                                backgroundColor: Colors.red,
+                                colorText: Colors.white,
+                              );
+                            } else if (passwordTextEditingController.text.length <
+                                6) {
+                              Get.snackbar(
+                                "Error!",
+                                "Password must be atleast 6 character",
+                                snackPosition: SnackPosition.BOTTOM,
+                                backgroundColor: Colors.red,
+                                colorText: Colors.white,
+                              );
+                            } else {
+                              registerNewUser(context);
+                            }
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Get.offAllNamed(AppRoutes.SIGNINSCREEN);
+                      print("clicked to go login");
+                    },
+                    child: Text(
+                      "Already have an Account? Login Here.",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ],
               ),
-              TextButton(
-                onPressed: () {
-                  Get.offAllNamed(AppRoutes.SIGNINSCREEN);
-                  print("clicked to go login");
-                },
-                child: Text(
-                  "Already have an Account? Login Here.",
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
