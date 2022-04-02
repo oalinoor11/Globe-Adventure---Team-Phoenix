ee48ccc7cf748869a6462aab3f6270ab251a7bea
oalinoor11 <oalinoor11@gmail.com>
2022-04-02 09:26:28 +0600

    responsive done on college

--- lib/Presentation/AllScreen/College/CollegeScreen.dart
+++ lib/Presentation/AllScreen/College/CollegeScreen.dart
@@ -15,110 +15,116 @@ import 'package:get/get_navigation/src/extension_navigation.dart';
 class CollegeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: Center(
-          child: Text(
-            "College",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        automaticallyImplyLeading: false,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                viewportFraction: 1.0,
-                autoPlay: false,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
+    return Center(
+      child: Container(
+        decoration: context.width > 550 ? BoxDecoration(border: Border.all(width: 2, color: Colors.grey.withOpacity(0.35))): null,
+        width: context.width > 550 ? 550 : double.infinity,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            centerTitle: true,
+            title: Center(
+              child: Text(
+                "College",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              items: [1].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: double.infinity,
-                      child: Image.asset(
-                        "assets/collegebanner.png",
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
             ),
-            SizedBox(height: 18),
-            StreamBuilder(
-                builder: (context, AsyncSnapshot<List<CountryModel>> snapshot) {
-                  if (snapshot.hasData) {
-                    return GridView.builder(
-                      padding: EdgeInsets.symmetric(horizontal: 18.0),
-                      primary: false,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        CountryModel country = snapshot.data![index];
-                        return InkWell(
-                          child: Container(
-                            decoration: new BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.green, width: 1.5),
-                              boxShadow: [
-                                new BoxShadow(
-                                  color: Colors.grey.withOpacity(0.15),
-                                  blurRadius: 5.0,
-                                ),
-                              ],
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Column(
-                              children: [
-                                Container(
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(15),
-                                    child: Image(
-                                      image: NetworkImage(country.countryFlag),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(height: 5),
-                                Text(country.countryName,
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                                SizedBox(height: 5),
-                              ],
-                            ),
+            automaticallyImplyLeading: false,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                CarouselSlider(
+                  options: CarouselOptions(
+                    viewportFraction: 1.0,
+                    autoPlay: false,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                  ),
+                  items: [1].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: double.infinity,
+                          child: Image.asset(
+                            "assets/collegebanner.png",
+                            fit: BoxFit.cover,
                           ),
-                          onTap: () {
-                            CollegeController.to
-                                .selectedCountry(country.countryName);
-                            Get.toNamed(AppRoutes.COLLEGESCREEN2,
-                                arguments: country);
-                          },
                         );
                       },
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                        childAspectRatio: 0.95,
-                        crossAxisCount: context.width > 1080 ? 4 : 3,
-                      ),
-                      // itemCount: (snapshot.data as QuerySnapshot).documents.length,) ,
-                      itemCount: snapshot.data?.length ?? 0,
                     );
-                  } else {
-                    return CircularProgressIndicator();
-                  }
-                },
-                stream: CountryModel.getCountries()),
-          ],
+                  }).toList(),
+                ),
+                SizedBox(height: 18),
+                StreamBuilder(
+                    builder: (context, AsyncSnapshot<List<CountryModel>> snapshot) {
+                      if (snapshot.hasData) {
+                        return GridView.builder(
+                          padding: EdgeInsets.symmetric(horizontal: 18.0),
+                          primary: false,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            CountryModel country = snapshot.data![index];
+                            return InkWell(
+                              child: Container(
+                                decoration: new BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.green, width: 1.5),
+                                  boxShadow: [
+                                    new BoxShadow(
+                                      color: Colors.grey.withOpacity(0.15),
+                                      blurRadius: 5.0,
+                                    ),
+                                  ],
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(15),
+                                        child: Image(
+                                          image: NetworkImage(country.countryFlag),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(country.countryName,
+                                        textAlign: TextAlign.center,
+                                        style:
+                                            TextStyle(fontWeight: FontWeight.bold)),
+                                    SizedBox(height: 5),
+                                  ],
+                                ),
+                              ),
+                              onTap: () {
+                                CollegeController.to
+                                    .selectedCountry(country.countryName);
+                                Get.toNamed(AppRoutes.COLLEGESCREEN2,
+                                    arguments: country);
+                              },
+                            );
+                          },
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 0.95,
+                            crossAxisCount: 3,
+                          ),
+                          // itemCount: (snapshot.data as QuerySnapshot).documents.length,) ,
+                          itemCount: snapshot.data?.length ?? 0,
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    },
+                    stream: CountryModel.getCountries()),
+              ],
+            ),
+          ),
         ),
       ),
     );
